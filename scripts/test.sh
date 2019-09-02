d12b5a504022f4aee87b6e42d1d197f418438a86
Shmuela Jacobs <shmool@users.noreply.github.com>
2019-09-02 07:50:35 -0400

    Add quotes to $CWD to allow paths with spaces. (#49)
    Fixes #48

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,18 +9,18 @@ AZURE_SUBSCRIPTION=${1:'ca-yolasors-demo-test'}
 AZURE_RESOURCE_GROUP='ci-azure-ng-deploy'
 AZURE_STORAGE='ciazurengdeploy'
 CWD=`pwd`
-TEST_FOLDER=$CWD/.e2e-tests
+TEST_FOLDER="$CWD/.e2e-tests"
 
 function cleanup() {
-    cd $CWD
-    rm -rf $TEST_FOLDER
+    cd "$CWD"
+    rm -rf "$TEST_FOLDER"
 }
 
 # Cleanup test folder in case of error
 trap cleanup ERR
 
-mkdir -p $TEST_FOLDER
-cd $TEST_FOLDER
+mkdir -p "$TEST_FOLDER"
+cd "$TEST_FOLDER"
 
 echo
 echo -------------------------------------------------------------------------------
@@ -37,8 +37,8 @@ npm i -D ../azure-ng-deploy*.tgz
 ng add @azure/ng-deploy -m true -n $AZURE_SUBSCRIPTION -g $AZURE_RESOURCE_GROUP -a $AZURE_STORAGE -l "westus" --telemetry false
 ng build --prod
 ng run sample-app:deploy
-cd $CWD
-rm -rf $TEST_FOLDER
+cd "$CWD"
+rm -rf "$TEST_FOLDER"
 
 # Cleanup resource group using az cli
 az.cmd group delete -n $AZURE_RESOURCE_GROUP -y
